19237e6be1c89096c1a71d6a8aea142c1991cf62
ioloaa <seregabogdashov@gmail.com>
2026-07-30 01:42:47 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,12 @@
+from telegram import Update
 
+from core.anti_flood import anti_flood
+
+
+async def admin_only(update: Update) -> bool:
+    """Проверяет, является ли пользователь админом"""
+    user_id = update.effective_user.id
+    if not anti_flood.is_admin(user_id):
+        await update.message.reply_text("Только для админов. Ты кто такой?")
+        return False
+    return True
